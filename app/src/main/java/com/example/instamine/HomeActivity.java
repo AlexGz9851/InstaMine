beb0bebefa169fb46c1a1d5b33f36e1f3b0adba5
alexgz9851 <zzz12300212@gmail.com>
2019-07-09 17:53:13 -0700

    Toolbars developed with fragments. Stylish HomePage

--- app/src/main/java/com/example/instamine/HomeActivity.java
+++ app/src/main/java/com/example/instamine/HomeActivity.java
@@ -4,14 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import com.parse.ParseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
     ParseUser user;
-    TextView dummyTx;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/example/instamine/ToolbarFragment.java
@@ -0,0 +1,25 @@
+package com.example.instamine;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ToolbarFragment extends Fragment {
+
+
+    // it is called when you want create the layout of the fragment. Dinamically or using an XML made.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+            return inflater.inflate(R.layout.fragment_toolbar, parent,false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    }
+}
